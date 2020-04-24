0964195e274bd6f3ef65eeaa14ede26362eef934
Pitter <ikevin77@hotmail.fr>
2020-04-24 14:10:19 +0200

    coffret

---
+++ GG Week/Assets/Scripts/Kevin_Script/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip[] son;
+    public int number;
+
+     /*public static void playSound()
+     {
+         GameObject soundGameObject = new GameObject("son");
+         AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+
+     }*/  
+
+    void Update()
+    {
+
+        int j = number;
+        for (int i = 0; i < son.Length; i++)
+        {
+            if (i == j)
+            {
+                //son[i];
+            }
+        }
+    }
+
+}
